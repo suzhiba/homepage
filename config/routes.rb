5f327c106dd38f978414d2556126443a4972e9bc
Adam Waxman <awaxman11@gmail.com>
2013-05-20 03:21:11 -0400

    Finished basic terminal

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 PersonalHomepage::Application.routes.draw do
+  root to: 'static_pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+ 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
